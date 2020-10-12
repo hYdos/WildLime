eeaa673a74bf07bb2031be6d1c3cc906f753480a
hydos <haydenv06@gmail.com>
2020-10-12 17:19:56 +1000

    initial wildLime stuff

---
+++ cmake-build-debug/CMakeFiles/WildLime.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WildLime.dir/main.cpp.o"
+  "WildLime"
+  "WildLime.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WildLime.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
